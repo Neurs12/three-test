aaa424eab35e4fefdbdebcd5083575689f71b658
Neurs <hoanghuutrung431@gmail.com>
2024-01-05 17:29:10 +0700

    Again... 😇

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: process.env.NODE_ENV === "production" ? "export" : undefined,
     basePath: process.env.NODE_ENV === "production" ? "/three-test" : undefined,
-    assetPrefix: process.env.NODE_ENV === "production" ? "/three-test" : undefined,
+    trailingSlash: true,
 }
 
 module.exports = nextConfig
