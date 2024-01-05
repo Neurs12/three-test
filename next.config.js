8864c664a3e4e8a586606a5224a8be783041c340
Neurs <hoanghuutrung431@gmail.com>
2024-01-05 17:59:24 +0700

    Final final??? 🗣️🔥🔥

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
     output: process.env.NODE_ENV === "production" ? "export" : undefined,
-    basePath: process.env.NODE_ENV === "production" ? "/three-test" : undefined,
-    trailingSlash: true,
 }
 
 module.exports = nextConfig
